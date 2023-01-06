fa3fae8ad52bf42a27f70f595dbc8ef66d6bd969
yugmani <yugmani@yahoo.com>
2023-01-05 23:21:04 -0800

    scope and closures in nested functions

--- index.js
+++ index.js
@@ -30,3 +30,45 @@ name();
 //global scope
 console.log(num);
 console.log(myName);
+
+//scope in nested function
+// ***************************
+
+let a = 10;
+
+function outer() {
+  let b = 20;
+
+  function inner() {
+    let c = 30;
+    console.log(a, b, c);
+  }
+
+  inner(); //invoking nested function
+}
+
+//invoke outer function
+outer(); //10 20 30
+
+//Closure
+// **********************
+
+function parent() {
+  let counter = 0;
+
+  function child() {
+    counter++;
+    console.log(counter);
+  }
+
+  // child();
+  return child;
+}
+
+//invoke parent function
+// parent(); //1
+// parent(); //1
+
+const fn = parent();
+fn(); //1
+fn(); //2 //?
